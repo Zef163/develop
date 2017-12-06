d85095a47544a3c010f81b134009382d9716a5b2
Andrey Rogachev <vi163rus@gmail.com>
2017-12-06 11:50:38 +0400

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js"
     },
     "testMatch": [
-        "**/__jest__/**/*.js?(x)",
-        "**/?(*.)(spec|test).js?(x)"
-    ]
+        "<rootDir>/test/__jest__/**/*.js?(x)"
+    ],
+    "testPathIgnorePatterns": []
 };
